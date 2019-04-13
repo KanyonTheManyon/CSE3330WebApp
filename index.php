f1ab70e72a9c85630171c409afa8f93ad392cfa3
Kanyon Loyd <43161994+KanyonTheManyon@users.noreply.github.com>
2019-04-13 17:41:37 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Web App</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body style=background-color:#bdc437>
+    <div id="main-wrapper">
+        <center><h2>Search our World Cup Database!</h2></center>
+
+        <div class="inner-form">
+            <form action="index.php" method="post">
+
+            <label><b>Country Name</b></label>
+            <input type="text" placeholder="Enter Country Name" name=CountryName>
+            <label><b>Color of Card (Optional)</b></label>
+        </div>
+    </div>
+
+</body>
+</html>
